bd36a346e98100b01d3df57e5ccaa6dd537d1366
FaizRozaqi <faiz.m.rozaqi@gdplabs.id>
2021-06-23 08:41:28 +0700

    end section react-redux

--- src/app.js
+++ src/app.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter from './routers/AppRouter'
-import 'normalize.css/normalize.css';
-import './styles/style.scss';;
+import { Provider } from 'react-redux';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import getVisibleExpenses from './selectors/expenses';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+import 'normalize.css/normalize.css';
+import './styles/style.scss';
+
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500}));
+store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000}));
+store.dispatch(addExpense({ description: 'Rent', amount: 109500}));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+console.log(visibleExpenses);
+
+const jsx = (
+    <Provider store= {store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
